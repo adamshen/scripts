6e967b4ad91df9ace9523faab8cc17fbcb3fe3d4
adamshen <adam_ruby@126.com>
2017-03-06 13:19:47 +0800

    add ruby dep-packets

--- ubuntu/rbenv.sh
+++ ubuntu/rbenv.sh
@@ -3,3 +3,4 @@ cd ~/.rbenv && src/configure && make -C src
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
